f7cca76c7b9b883cfc4fa4db1d2297dab718dcd6
Deborah Chizim <deborah@Deborahs-MacBook-Pro.local>
2019-01-10 00:00:09 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io').listen(server);
+
+var usernames = [];
+
+server.listen(process.env.PORT || 3000); 
+
+app.get('/', (req, res)=>{
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.sockets.on('connection', (socket)=>{
+
+    socket.on('new user', (data, callback)=>{
+        if(usernames.indexOf(data) != -1){
+            callback(false);
+        }
+        else{
+            callback(true);
+            socket.username = data;
+            usernames.push(socket.username);
+            updateUsernames();
+        }
+    });
+
+    function updateUsernames(){
+        io.sockets.emit('usernames', usernames);
+    }
+
+    socket.on('send message', (data)=>{
+        io.sockets.emit('new message', {msg: data, user: socket.username});
+    });
+
+    socket.on('disconnect', (data)=>{
+        if(!socket.username){
+            return;
+        }
+
+        usernames.splice(usernames.indexOf(socket.username), 1);
+        updateUsernames();
+    })
+});
